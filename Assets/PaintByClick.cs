f5e2878782b3a3e9665d8fed69be2f7787955bb1
kokole130 <kokole130@gmail.com>
2020-12-24 20:53:29 +0200

    few changes

--- Assets/PaintByClick.cs
+++ Assets/PaintByClick.cs
@@ -6,12 +6,18 @@ public class PaintByClick : MonoBehaviour
 {
     [SerializeField] private Color cur_color = new Color();
     [SerializeField] private Vector3 mouse_pos;
-
-
+    [SerializeField]
+    GameObject[] slider_array;
+    bool mixActive;
     // Start is called before the first frame update
     void Start()
     {
-
+        mixActive = false;
+        slider_array = GameObject.FindGameObjectsWithTag("slider");
+        for(int i = 0; i < slider_array.Length; i++)
+        {
+            slider_array[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -26,7 +32,7 @@ public class PaintByClick : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log("if 2");
-                if (hit.transform.tag == "color")
+                if (hit.collider != null && hit.transform.tag == "color")
                 {
                     Debug.Log("dasdlamdsaldmasksda");
                     Debug.Log(hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color"));
@@ -37,6 +43,25 @@ public class PaintByClick : MonoBehaviour
                 if(hit.collider !=null && hit.transform.tag == "paintable")
                 {
                     hit.collider.gameObject.GetComponent<SpriteRenderer>().color = cur_color;
+                }
+
+                if (hit.collider != null && hit.transform.tag == "uncolor")
+                {
+                    if (!mixActive) {
+                        for (int i = 0; i < slider_array.Length; i++)
+                        {
+                            slider_array[i].SetActive(true);
+                        }
+
+                    }
+                    else
+                    {
+                        cur_color = hit.collider.gameObject.GetComponent<newMixedColor>().curr;
+                        for (int i = 0; i < slider_array.Length; i++)
+                        {
+                            slider_array[i].SetActive(false);
+                        }
+                    }
                 }
             }
 
---
+++ Assets/newMixedColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newMixedColor : MonoBehaviour
+{
+    public Color curr;
+    float R, G, B;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        R = 255f;
+        G = 255f;
+        B = 255f;
+        curr = new Color(R, G, B);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curr = new Color(R, G, B);
+    }
+
+    public void addRedColor(float add)
+    {
+        R = add;
+    }
+    public void addGreenColor(float add)
+    {
+        G = add;
+    }
+    public void addBlueColor(float add)
+    {
+        B = add;
+    }
+}
