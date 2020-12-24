f58ad0aac2f820a8274f1fec6127be6b07d05f23
yogev100 <yogev2468@gmail.com>
2020-12-24 17:44:08 +0200

    update level 1

---
+++ Assets/PaintByClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintByClick : MonoBehaviour
+{
+    [SerializeField] private Color cur_color = new Color();
+    [SerializeField] private Vector3 mouse_pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("if 1");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("if 2");
+                if (hit.transform.tag == "color")
+                {
+                    Debug.Log("dasdlamdsaldmasksda");
+                    Debug.Log(hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color"));
+                    cur_color = hit.collider.gameObject.GetComponent<Renderer>().material.GetColor("_Color");
+                }
+                Debug.Log(hit.transform.tag);
+
+                if(hit.collider !=null && hit.transform.tag == "paintable")
+                {
+                    hit.collider.gameObject.GetComponent<SpriteRenderer>().color = cur_color;
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/setColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setColor : MonoBehaviour
+{
+    [SerializeField] 
+    [Range(0f,256f)]
+    float R = 255f;
+    [SerializeField]
+    [Range(0f, 256f)]
+    float G = 255f;
+    [SerializeField]
+    [Range(0f, 256f)]
+    float B = 255f;
+    SpriteRenderer cubeRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get the Renderer component from the new cube
+        cubeRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Call SetColor using the shader property name "_Color" and setting the color to red
+        cubeRenderer.material.SetColor("_Color", new Color(R/255f,G / 255f, B / 255f));
+    }
+}
